b83740e4b288bd2e2862cf9c90f95f9df5b1693a
Manisha Kumari <manishakumari@Manishas-MacBook-Air.local>
2026-01-19 16:31:43 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,22 @@
+function scrollToSection(id) {
+            const element = document.getElementById(id);
+            if (element) {
+                element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+            }
+            const navbar = document.getElementById('navbar');
+            navbar.classList.remove('active');
+        }
+
+        function toggleMenu() {
+            const navbar = document.getElementById('navbar');
+            navbar.classList.toggle('active');
+        }
+        document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+            anchor.addEventListener('click', function (e) {
+                e.preventDefault();
+                const target = document.querySelector(this.getAttribute('href'));
+                if (target) {
+                    target.scrollIntoView({ behavior: 'smooth' });
+                }
+            });
+        });
